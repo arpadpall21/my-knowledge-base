f439e4161eed7fbedf7b77c15fc0e09de4e2f797
arpadpall21 <a.pall21@yahoo.fr>
2022-03-12 19:52:32 +0100

    2022-03-12

--- My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.js
+++ My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.js
@@ -1,7 +1,24 @@
 class TestClass {
-    #private;
-    
-    getPrivate(){
-        return this#private
-    }
+    method(){ return 'stMethod-val' };
+
+    prop = 'stProp1-val';
 }
+
+class SubClass extends TestClass{}
+
+
+testObj = new TestClass()
+subObj = new SubClass()
+
+console.log( testObj.prop )
+console.log( subObj.prop )
+console.log( testObj.method() )
+console.log( subObj.method() )
+
+// console.log( TestClass.stProp )
+// console.log( TestClass.stMethod() )
+
+// console.log( SubClass.stProp )
+// console.log( SubClass.stMethod() )
+
+
