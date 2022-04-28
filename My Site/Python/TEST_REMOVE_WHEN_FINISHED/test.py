0bba6d8586504c6eb536b4ecda9b38faffb20199
arpadpall21 <a.pall21@yahoo.fr>
2022-04-28 22:12:13 +0200

    2022-04-28

--- My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
+++ My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
@@ -1,2 +1,15 @@
-with 'test':
-    raise Exception()
+from contextlib import contextmanager
+
+@contextmanager
+def ctxmanager():
+    print('setup---------')
+    yield "Hello World!"
+    print('teardown------')
+    return True
+
+with ctxmanager() as alias:
+    print('ctx manger context ->')
+    print( alias )
+    raise Exception('my exception')
+
+print( '---------------------------after with')
