f1613230d168684c603ba07288ccb3e99110d577
arpadpall21 <a.pall21@yahoo.fr>
2022-05-11 09:43:22 +0200

    2022-05-11

--- My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
+++ My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
@@ -1,3 +1,20 @@
-print(
-    [1, 2, 3] == {1, 2, 3}
-)
+import asyncio
+
+async def delay(delay):
+    await asyncio.sleep(delay)
+    print( f'Delayed {delay} seconds')
+    return 'OK'
+
+async def main():
+    # loop = asyncio.get_event_loop()
+    # future = loop.create_future()
+    
+    
+    task1 = asyncio.create_task(delay(3))
+    task2 = asyncio.create_task(delay(3))
+    task3 = asyncio.create_task(delay(3))
+# 
+    print( await asyncio.wait([task1, task2, task3]) )
+
+# asyncio.create_future()
+asyncio.run(main())
