d2539718ed41f24f7eb2980cb4379749dfec6e19
arpadpall21 <a.pall21@yahoo.fr>
2022-05-13 19:27:39 +0200

    2022-05-13

--- My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
+++ My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
@@ -1,20 +1,21 @@
 import asyncio
 
-async def delay(delay):
-    await asyncio.sleep(delay)
-    print( f'Delayed {delay} seconds')
-    return 'OK'
+def someFn():
+    return 'function result'
+
+# async def delay(delay, error=False):
+#     await asyncio.sleep(delay)
+#     print( f'Delayed {delay} seconds')
+    
+#     if error:
+#         raise Exception('some errror')
+    
+#     return f'OK delay {delay}'
+
+def main():
+    
 
 async def main():
-    # loop = asyncio.get_event_loop()
-    # future = loop.create_future()
-    
-    
-    task1 = asyncio.create_task(delay(3))
-    task2 = asyncio.create_task(delay(3))
-    task3 = asyncio.create_task(delay(3))
-# 
-    print( await asyncio.wait([task1, task2, task3]) )
 
-# asyncio.create_future()
+
 asyncio.run(main())
