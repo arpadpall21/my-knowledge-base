eb35412c2497c6150f8df9a5f53984f9484c2d73
arpadpall21 <a.pall21@yahoo.fr>
2022-07-18 20:55:31 +0200

    2022-07-18

--- My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
+++ My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
@@ -1,14 +1,99 @@
-from abc import ABC, abstractclassmethod
+# import threading
+# import time
 
-class AbcCls(ABC):
-    @abstractclassmethod
-    def baseMethod(self):
-        return 'base value'
+# USE_DAEMON = False
+
+# r = 10000000
+# start = time.time()
+
+# def countrange(name, len):
+#     for i in range(len + 1):
+#         if i == r / 4:
+#             print( f"{name}: {i}" )
+#         elif i == r / 2:
+#             print( f"{name}: {i}" )
+#         elif i == r / 4 * 3:
+#             print( f"{name}: {i}" )
+#         elif i == r:
+#             print( f"{name}: {i}" )
+
+# def th(name):
+#     print(f'{name} started')
+#     countrange(name, r)
+#     print(f'{name} end!')
 
 
-class TestClass(AbcCls):
-    def baseMethod(self):
-        return super().baseMethod()
-        
-inst = TestClass()
-print( inst.baseMethod() )
+# a = threading.Thread(target=th, args=["Thread-1"], daemon=USE_DAEMON)
+# b = threading.Thread(target=th, args=["Thread-2"], daemon=USE_DAEMON)
+# c = threading.Thread(target=th, args=["Thread-2"], daemon=USE_DAEMON)
+# d = threading.Thread(target=th, args=["Thread-2"], daemon=USE_DAEMON)
+
+# a.start()
+# b.start()
+# c.start()
+# d.start()
+
+# a.join()
+# b.join()
+# c.join()
+# d.join()
+
+# # countrange("Main", r)
+
+
+
+# print( f"Running time: {time.time() - start}" )
+
+
+
+###############################################################################################
+###############################################################################################
+###############################################################################################
+
+# import concurrent.futures
+# import time
+
+# r = 10000000
+# start = time.time()
+
+# def countrange(name, len):
+#     for i in range(len + 1):
+#         if i == r / 4:
+#             print( f"{name}: {i}" )
+#         elif i == r / 2:
+#             print( f"{name}: {i}" )
+#         elif i == r / 4 * 3:
+#             print( f"{name}: {i}" )
+#         elif i == r:
+#             print( f"{name}: {i}" )
+
+# def th(name, b):
+#     print(f'{name} started')
+#     countrange(name, r)
+#     print(f'{name} end!')
+#     print( f'2nd argument {b}')
+
+
+
+# with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+#     executor.map(th, ['Thread-1', 'Thread-2', 'Thread-3'], ['x', 'y', 'z'])
+
+
+
+
+# print( f"Running time: {time.time() - start}" )
+
+
+###############################################################################################
+###############################################################################################
+###############################################################################################
+
+import threading
+
+lock = threading.Lock()
+lock.acquire()
+
+print( lock.locked() )
+
+lock.release()
+print( lock.locked() )
