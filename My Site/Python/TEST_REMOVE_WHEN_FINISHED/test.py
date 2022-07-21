448464a5177856ec3226bf6228d039387f394ee3
arpadpall21 <a.pall21@yahoo.fr>
2022-07-21 07:42:55 +0200

    2022-07-21

--- My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
+++ My Site/Python/TEST_REMOVE_WHEN_FINISHED/test.py
@@ -88,12 +88,120 @@
 ###############################################################################################
 ###############################################################################################
 
+# from concurrent.futures import ThreadPoolExecutor
+# import threading
+
+# lock = threading.RLock()
+# threadpolExecutor = ThreadPoolExecutor(max_workers=5)
+
+# def countrange(name, loop):
+#     if loop:
+#         for i in range(10000000):
+#             pass
+
+# def th(name, lock, loop=True):
+#     print(f'{name} started')
+#     with lock:                      # lock supports context protocol 
+#         countrange(name, loop)      # lock unlocket at the start of the context and locked at the end of it
+#     print(f'{name} end!')
+
+# threadpolExecutor.map(th, ['th-1', 'th-2', 'th-3', 'th-4', 'th-5'], [lock, lock, lock, lock, lock], [False, False, True, False, True])   # start threads
+# # threads run concurently but have to wait each other to finish
+
+
+###############################################################################################
+###############################################################################################
+###############################################################################################
+
+# deadlock 
+
+# import threading 
+
+# lock = threading.Lock()
+
+# print("before first acquire")
+# lock.acquire()
+# print("before second acquire")
+# lock.acquire()                        # -! program will hang because tries to acquire the locked lock (this is why using the context protocol is highli advised)
+# print("acquired lock twice")
+
+
+###############################################################################################
+###############################################################################################
+###############################################################################################
+
+# import threading
+
+# s = threading.Semaphore(0)
+# bs = threading.BoundedSemaphore(0)
+
+# print( s._value )
+# s.release()
+# s.release()
+# print( s._value )
+
+# print( bs._value )
+# # bs.release()
+# # bs.release()
+# print( bs._value )
+
+
+###############################################################################################
+###############################################################################################
+###############################################################################################
+
+# import threading
+
+# def countrange():
+#     print( threading.enumerate() )
+#     print( threading.active_count() )
+#     for i in range(10000000):
+#         pass
+
+# t = threading.Timer(5, countrange)
+# t.start()
+
+
+###############################################################################################
+###############################################################################################
+###############################################################################################
+
+# from concurrent.futures import ThreadPoolExecutor
+# import threading
+# import time
+
+# executor = ThreadPoolExecutor(max_workers=2)
+# b = threading.Barrier(2)
+
+# # def th():
+# #     print( 'th-1 start:' )
+# #     time.sleep(1)
+# #     b.wait()
+# #     print( 'th-1 end:' )
+
+# def th(name, wait):
+#     print( f'{name} start' )
+#     time.sleep(wait)
+#     b.wait()
+#     print( f'{name} end' )
+
+# executor.map(th, [1, 2, 3, 4, 5], [2, 5, 1.2, 1.1, 4.2])
+
+
+###############################################################################################
+###############################################################################################
+###############################################################################################
+
 import threading
 
-lock = threading.Lock()
-lock.acquire()
+barrier = threading.Barrier(3, timeout=2)
 
-print( lock.locked() )
+def count(name, len, barrier):
+    for i in range(len):
+        pass
+    barrier.wait()
+    print( f'{name} thread end!')
 
-lock.release()
-print( lock.locked() )
+threading.Thread(target=count, args=['th1', 10000000, barrier]).start()
+threading.Thread(target=count, args=['th2', 100000, barrier]).start()
+threading.Thread(target=count, args=['th3', 1000000000, barrier]).start()
