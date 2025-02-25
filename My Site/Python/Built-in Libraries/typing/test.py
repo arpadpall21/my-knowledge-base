b8f7b8222e2745a3a04bc680bb1b26b68e569a3c
Arpad Pall <a.pall21@yahoo.fr>
2025-02-25 19:52:43 +0100

    2025-02-25

--- My Site/Python/Built-in Libraries/typing/test.py
+++
@@ -1,5 +0,0 @@
-import calendar
-
-# Find out the day of the week for July 4, 2023
-day_of_week = calendar.weekday(2023, 7, 4)
-print(f"The day of the week for July 4, 2023 is: {calendar.day_name[day_of_week]}")
