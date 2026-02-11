c7eeba508aaf54d05d6899fd57da78926632c9a5
arpadpall21 <a.pall21@yahoo.fr>
2026-02-11 09:15:00 +0100

    2026-02-11

--- My Site/Pandas/TEST/main.py
+++ My Site/Pandas/TEST/main.py
@@ -1,153 +1,33 @@
 import pandas as pd
 
-coffe_shop = pd.DataFrame({                                                                           
-        'Day': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        'Coffee': ['Latte', 'Espresso', 'Cappuccino', 'Americano', 'Mocha', 'Flat White', 'Macchiato'],
-        'Units Sold': [120, 150, 200, 180, 220, 160, 140],
-    })
+comp_1 = pd.DataFrame({
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'Departement': ['HR', 'IT', 'HR', 'Finance', 'IT'],
+})
 
-    ### accessing by  label
-# print( coffe_shop["Units Sold"] )    # returns the 'Units Sold' column as a Series
-# print( coffe_shop[["Day", "Coffee"]] )   # returns the 'Day' and 'Coffee' columns as a DataFrame
+comp_2 = pd.DataFrame({
+    'Name': ['Johon', 'Betty', 'Maggie', 'Jeny'],
+    "DP": ['HR', 'Finance', 'Accounting', 'HR'],
+})
 
-#     # indices also can be used as labels
-# print( coffe_shop.loc[0])                 # returns the 1st row as a Series
-# print( coffe_shop.loc[0:4])               # returns the 1st to 5th rows as a DataFrame
-# print( coffe_shop.loc[[2, 3]])            # returns the 3rd and 4th rows as a DataFrame
-
-# print( coffe_shop.loc[0, ["Day"]])        # returns the value of the 'Day' column for the 1st row
-# print( coffe_shop.loc[0:2, ["Day", "Units Sold"]])   # returns the 'Day' and 'Units Sold' columns for the 1st to 3rd rows as a DataFrame (slicing data by label)
-
-coffe_shop.rename(index=["mon", "tue", "wed", "thu", "fri", "sat", "sun"], inplace=True)
-print( coffe_shop.loc["mon":"fri", ["Day", "Coffee"]] )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-####################################################################
-# sort
-####################################################################
-# import pandas as pd
-
-# coffe_shop = pd.DataFrame({
-#     'Day': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-#     'Coffee': ['Latte', 'Espresso', 'Cappuccino', 'Americano', 'Mocha', 'Flat White', 'Macchiato'],
-#     'Units Sold': [120, 150, 200, 180, 220, 160, 140]
-# })
-
-# print(coffe_shop.sort_values("Units Sold"))   # returns a new DataFrame sorted by 'Units Sold' in ascending order
-# print(coffe_shop.sort_values("Units Sold", ascending=False))  # returns a new DataFrame sorted by 'Units Sold' in descending order
-
-# print(coffe_shop.sort_values(["Units Sold", "Day"]))  # sorts by 'Units Sold' and then by 'Day' for ties
-
-# print(coffe_shop)             # original DataFrame remains unchanged
-
-
-
-####################################################################
-# filter
-####################################################################
-# import pandas as pd
-# from pathlib import Path
-
-# olympic_data = pd.read_excel(Path() / 'olympics-data.xlsx')
+comp_2_unique = comp_2.drop_duplicates(subset="DP") 
 
 # print(
-#     olympic_data[olympic_data['height_cm'] > 200]     # returns a new filtered DataFrame with rows where 'height_cm' > 200
+#     pd.merge(comp_1, comp_2, left_on="Departement", right_on="DP", how="left")
 # )
-
-# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.query.html
 # print(
-#     olympic_data.query('height_cm > 200 and born_country == "FRA"')     # returns a new filtered DataFrame with rows where 'height_cm' > 200 and 'born_country' is 'FRA'
+#     pd.merge(comp_1, comp_2, left_on="Departement", right_on="DP", how="right")
 # )
-
-
 # print(
-#     olympic_data[(olympic_data['height_cm'] > 200) & (olympic_data['born_country'] == "FRA")]     # returns a new filtered DataFrame with rows where 'height_cm' > 200 and 'born_country' is 'FRA'
+#     pd.merge(comp_1, comp_2, left_on="Departement", right_on="DP", how="inner")
 # )
-
-# print(
-#     olympic_data.loc[olympic_data['born_country'] == "USA", ["name", "born_city"]]   # returns a new filtered DataFrame with specific columns where 'born_country' is 'USA'
-# )
-
-# print(
-#     olympic_data.loc[olympic_data['born_country'] == "USA"].sort_values('height_cm', ascending=False).head(5)   # returns a new filtered and sorted DataFrame 
-# )
-
-# the str.contains(regExp)    accepts regular expressions Document this
-# https://pandas.pydata.org/docs/reference/api/pandas.Series.str.contains.html (series method to filter)
-# print(
-#     olympic_data.loc[olympic_data["name"].str.contains("tommy", case=False)],       # returns a new filtered DataFrame with rows where 'name' contains 'Tommy'
-# )
+print(
+    pd.merge(comp_1, comp_2, left_on="Departement", right_on="DP", how="outer")
+)
 
 
 
 
-####################################################################
-# apply()
-####################################################################
-# import pandas as pd
-
-# olympic_data = pd.read_excel("olympics-data.xlsx")
-
-# olympic_data["medals_won"] = "Gold"   # add new column with default value
-
-
-
-# # document this https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.apply.html (apply method)
-# # def continent_condition(row):
-# #     if row['born_country'] == 'USA':
-# #         return 'America'
-# #     else:
-# #         return 'Not America'
-
-# # olympic_data["continent"] = olympic_data.apply(continent_condition, axis=1)  # add new column based on condition
-
-# olympic_data["height_inch"] = olympic_data.apply(lambda row: row["height_cm"] * 0.393701, axis=1)
-
-# print( olympic_data.head() )
-
-
-
-# # print( olympic_data.drop(columns=["died_date", "NOC"]) )  # returns a new DataFrame with specified columns dropped (does not modify original DataFrame)
-
-# # olympic_data.drop(columns=["died_date", "NOC"], inplace=True) # drops specified columns from the original DataFrame (modifies the original DataFrame)
-
-
-# print( olympic_data.head() )
-
-
-
-
-
-####################################################################
-# write to file
-####################################################################
-# import pandas as pd
-
-# olympic_data = pd.read_excel("olympics-data.xlsx")
-# olympic_data.to_csv("olympics-data.csv", index=False)    # write DataFrame to CSV file without the index column
-
-
-
-
-####################################################################
-# merge dataframes
-####################################################################
-# import pandas as pd
 
 # olympic_data_short = pd.read_csv("olympics-data-short.csv")
 # country_code_map = pd.read_csv("country-code-map.csv")
@@ -176,71 +56,6 @@ print( coffe_shop.loc["mon":"fri", ["Day", "Coffee"]] )
 
 
 ####################################################################
-# concat dataframes (do this stuff for example)
-####################################################################
-# import pandas as pd
-
-# olympic_data_short = pd.read_excel("olympics-data.xlsx")
-
-# hun_athletes = olympic_data_short[olympic_data_short["born_country"] == "HUN"].head(10)
-# rou_athletes = olympic_data_short[olympic_data_short["born_country"] == "ROU"].head(10)
-
-# hun_rou_athletes = pd.concat([hun_athletes, rou_athletes], ignore_index=True)   # concatenate two DataFrames into one, resetting the index
-
-
-
-
-####################################################################
-# handling null / missing values
-####################################################################
-# import pandas as pd
-
-# olympic_data = pd.read_excel("olympics-data.xlsx")
-
-
-# print(
-#     olympic_data_short.isna()          # returns a DataFrame of the same shape with boolean values indicating presence of null values
-# )
-
-# print(
-#     olympic_data_short.isna().sum()    # returns a Series with the count of null values in each column
-# )
-
-# new_olympic_data = olympic_data.head(100)
-
-# remove rows were there's any null values
-# print(
-#     new_olympic_data.dropna()
-# )
-# remove rows where a specific column has null values
-# print(
-#     new_olympic_data.dropna(subset="died_date")
-# )
-
-# print(
-#     new_olympic_data
-# )
-
-
-# # fill null values with a specified value
-# print(
-#     new_olympic_data.fillna("Unknown")
-# )
-
-
-# new_olympic_data = olympic_data.head(100)
-
-# # print(
-# #     new_olympic_data[new_olympic_data['died_date'].isna()]    # filter rows where 'died_date' is null
-# # )
-# print(
-#     new_olympic_data[new_olympic_data['died_date'].notna()]    # filter rows where 'died_date' is not null
-# )
-
-
-
-
-####################################################################
 # aggregation and grouping
 ####################################################################
 # import pandas as pd
@@ -255,12 +70,32 @@ print( coffe_shop.loc["mon":"fri", ["Day", "Coffee"]] )
 # )
 
 
-
 # print(
 #     olympic_data.groupby("born_country")["height_cm"].mean()  # returns a Series with average height for each country
 # )
 
 # DO THE PIVOT TABLE THING
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################################################
+# write to file
+####################################################################
+# import pandas as pd
+
+# olympic_data = pd.read_excel("olympics-data.xlsx")
+# olympic_data.to_csv("olympics-data.csv", index=False)    # write DataFrame to CSV file without the index column
 
 
 
@@ -291,7 +126,6 @@ print( coffe_shop.loc["mon":"fri", ["Day", "Coffee"]] )
 # print(revenue_df)
 
 
-
 ####################################################################
 # cumsum()
 ####################################################################
@@ -307,14 +141,6 @@ print( coffe_shop.loc["mon":"fri", ["Day", "Coffee"]] )
 # total_revenue = data["Revenue"].cumsum()   # returns a Series with the cumulative sum of the 'Revenue' column
 
 # print(total_revenue)
-
-
-
-
-
-
-
-
 
 
 ###################################################################################
